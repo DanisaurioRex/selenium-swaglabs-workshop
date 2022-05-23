e4150f1731e9b5056ac89ab3d82992a7e3449e27
djimenezo <daniel.jimenez@geniussports.com>
2022-05-23 10:00:27 -0500

    feat(reporter): add mochawesome reporter

--- src/infrastructure/chromeWebDriver.class.ts
+++ src/infrastructure/chromeWebDriver.class.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata'
 import { WebDriver, Builder } from 'selenium-webdriver'
 import { IDriver } from '../domain/driver.interface'
 import { injectable } from 'inversify'
+import { v4 as uuidv4 } from 'uuid'
+import fs from "fs"
 
 @injectable()
 export class SeleniumChromeWebDriver implements IDriver {
@@ -17,5 +19,16 @@ export class SeleniumChromeWebDriver implements IDriver {
 
     public async quit(): Promise<void> {
         await this.driver.quit()
+    }
+
+    public async takeScreenshot(): Promise<string> {
+        const reportPath = 'reports/'
+        const screenshotsFolder = 'assets/'
+        const fileName = `${uuidv4()}.png`
+
+        await fs.promises.mkdir(reportPath + screenshotsFolder, { recursive: true })        
+        const image = await this.driver.takeScreenshot()
+        await fs.promises.writeFile(reportPath + screenshotsFolder + fileName, image, 'base64')
+        return screenshotsFolder + fileName
     }
 }
